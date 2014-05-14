5a1868a62639aa600239d85aafe65ed2ea2bd280
Ashutosh Pandey <ashu1710@gmail.com>
2014-05-13 20:04:48 -0700

    Added a lot of new solutions.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 __author__ = 'ashutosh_pandey'
 
 def lastOfAList(l):
@@ -118,12 +120,104 @@ def replicate(l, n, original):
         rest = replicate(l, n - 1, original)
     return ([car] + rest)
 
-def dropEveryN(l, N):
-    pass
+def dropEveryN(l, N, original):
+    if l == []:
+        return []
+    if N == 1:
+        return dropEveryN(l[1:], original, original)
+    else:
+        car = l[0]
+        rest = dropEveryN(l[1:], N - 1, original)
+        return [car] + rest
+
+def splitParts(l, length):
+    if l == []:
+        return []
+
+    if length == 1:
+        return [[l[0]], l[1:]]
+    else:
+        car = l[0]
+        rest = splitParts(l[1:], length - 1)
+        rest[0] = [car] + rest[0]
+        return rest
+
+
+def slice(l, start, end):
+    if l == []:
+        return []
+    if end == 1:
+        return [l[0]]
+    else:
+        if start > 1:
+            return slice(l[1:], start - 1, end - 1)
+        elif start <= 1:
+            car = l[0]
+            rest = slice(l[1:], start - 1, end - 1)
+            return [car] + rest
+
+
+def rotateLeft(l, N):
+    return flatten(reverse(splitParts(l, N)))
+
+def removeKth(l, k):
+    if l == []:
+        return []
+    if k == 1:
+        return l[1:]
+    else:
+        car = l[0]
+        return [car] + removeKth(l[1:], k - 1)
+
+def insertAt(l, k, item):
+    if l == []:
+        return []
+    if k == 1:
+        return [item] + l
+    else:
+        car = l[0]
+        return [car] + insertAt(l[1:], k - 1, item)
+
+def inclusiveRange(start, end):
+    if start >= end:
+        return [start]
+    else:
+        return [start] + inclusiveRange(start + 1, end)
+
+def selectNRandom(l, N):
+    if l == []:
+        return []
+
+    index = random.randint(1, len(l))
+    if N == 1:
+        return [ l[index - 1] ]
+    else:
+        elem = l[index - 1]
+        return [elem] + selectNRandom(removeKth(l, index), N - 1)
+
+def lottoSelect(N, M):
+    return selectNRandom(inclusiveRange(1, M), N)
+
+def randomPermutation(l):
+    return selectNRandom(l, len(l))
+
+def combinations(l, n):
+    if l == []:
+        return []
+    if n == 1:
+        return [[x] for x in l]
+    else:
+        car = l[0]
+        withoutcar = combinations(l[1:], n)
+        withcar = combinations(l[1:], n - 1)
+
+        return withoutcar + [[car] + x for x in withcar]
+
 
 
 
 
 if __name__ == '__main__':
-    print dropEveryN([1, 2, 3, 4, 5], 3)
+    print combinations([1, 2, 3], 2)
+    # print lottoSelect(6, 500)
 
